959f835e31989a45f76587776cce9ec04704e268
Your Name <you@example.com>
2019-11-25 10:06:08 +0100

    gestions bus is clean

--- Telly/src/com/telly/controllers/BusController.java
+++ Telly/src/com/telly/controllers/BusController.java
@@ -18,34 +18,27 @@ import com.telly.service.BusService;
 @Controller
 public class BusController {
 
-	@Autowired
-	BusService busService;
-	
 
-	@RequestMapping("/createtrip")
-	public String reserveBus(Model model, Principal principal) {
+	@RequestMapping("/results")
+	public String leave(Model model, Principal principal) {
 
 		model.addAttribute("bus", new Bus());
 
-		return "createtrip";
+		return "results";
 	}
 
-	@RequestMapping(value = "/createreserve", method = RequestMethod.POST)
-	public String createReserve(@Validated(FormValidationGroup.class) Bus bus, BindingResult result,
+
+	@RequestMapping(value = "/resultsfrom", method = RequestMethod.GET)
+	public String leaveFrom(@Validated(FormValidationGroup.class) Bus bus, BindingResult result, Model model,
 			Principal principal) {
 
-		if (result.hasErrors()) {
-			return "reservebus";
-		}
+		List<Bus> results = busService.getCity(bus.getLeaveFrom(), bus.getGoingTo(), bus.getDateLeave(),
+				bus.getDateReturn());
+		model.addAttribute("results", results);
+		System.out.println(results);
 
-		busService.create(bus);
-
-		return "home";
+		return "results";
 
 	}
-
-
-
-
 
 }
